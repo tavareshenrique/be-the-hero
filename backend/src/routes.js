756f77fa14aaa4df7580cd4f2f5b7c929385ca4a
Henrique Tavares <ihenrits@gmail.com>
2020-03-25 17:59:49 -0300

    feat: ong controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,11 @@
 const express = require("express");
 
+const OngController = require("./controllers/OngController");
+
 const routes = express.Router();
+
+routes.get("/ongs", OngController.index);
+
+routes.post("/ongs", OngController.store);
 
 module.exports = routes;
