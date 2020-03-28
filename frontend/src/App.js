310c1df2e014a58cd5c30b4f318cbb1b7f8407ab
Henrique Tavares <ihenrits@gmail.com>
2020-03-28 00:38:13 -0300

    feat: add Toast, Alert and GlobalStyle in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
-import './global.css';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+
+import { ToastContainer } from 'react-toastify';
+
+import './config/ReactotronConfig';
+
+import AlertTemplate from 'react-alert-template-basic';
+
+import Header from '~/components/Header';
+import GlobalStyle from './styles/global';
 
 import Routes from './routes';
+
+const options = {
+  position: positions.TOP_CENTER,
+  timeout: 15000,
+  offset: '30px',
+  transition: transitions.SCALE,
+};
 
 function App() {
   return (
     <div className="App">
-      <Routes />
+      <AlertProvider template={AlertTemplate} {...options}>
+        <ToastContainer />
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </AlertProvider>
     </div>
   );
 }
