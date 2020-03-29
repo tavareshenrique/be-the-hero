1ea0602eb04c69957db8e4f2cc342ae06f04e6f2
Henrique Tavares <ihenrits@gmail.com>
2020-03-28 21:12:15 -0300

    feat: add eslint + editorconfig + prettier

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jsx-a11y', 'import', 'react-hooks', 'prettier'],
+
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'react/jsx-one-expression-per-line': 'off',
+    'react-native/no-raw-text': 'off',
+    'global-require': 'off',
+    'react-native/no-raw-text': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    camelcase: 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-props-no-spreading': [
+      1,
+      {
+        custom: 'ignore',
+      },
+    ],
+    'object-curly-newline': [
+      'warn',
+      {
+        multiline: true,
+        consistent: true,
+      },
+    ],
+    'operator-linebreak': [
+      'error',
+      'after',
+      { overrides: { '+=': 'before', '?': 'ignore', ':': 'ignore' } },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
+};
